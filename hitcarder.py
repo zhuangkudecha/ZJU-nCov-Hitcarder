ae0f0aab005f2a175b5044b9b2231b85d750421d
decha zhuangku <39823497+zhuangkudecha@users.noreply.github.com>
2022-10-12 12:23:29 +0800

    test

--- hitcarder.py
+++ hitcarder.py
@@ -192,12 +192,12 @@ def main(username, password):
     except Exception as err:
         return 1, '打卡登录失败：' + str(err)
 
-    try:
-        ret = hit_carder.check_form()
-        if not ret:
-            return 2, '打卡信息已改变，请手动打卡'
-    except Exception as err:
-        return 1, '获取信息失败，请手动打卡: ' + str(err)
+#     try:
+#         ret = hit_carder.check_form()
+#         if not ret:
+#             return 2, '打卡信息已改变，请手动打卡'
+#     except Exception as err:
+#         return 1, '获取信息失败，请手动打卡: ' + str(err)
 
     try:
         hit_carder.get_info()
